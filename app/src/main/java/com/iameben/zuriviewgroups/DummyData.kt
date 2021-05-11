57d28433d6122467fc345a6ccab8d5f66f347173
Ebenezer Justice <justiceebenezer4@gmail.com>
2021-05-11 03:38:03 +0100

    init

---
+++ app/src/main/java/com/iameben/zuriviewgroups/DummyData.kt
@@ -0,0 +1,31 @@
+package com.iameben.zuriviewgroups
+
+object DummyData {
+    val funFacts = listof(
+        FactModel(
+            "Kotlin",
+            R.drawable.kotlin_logo,
+            "This is for the kotlin Logo, quite alright!"
+        ),
+        FactModel(
+            "Java",
+            R.drawable.java_logo,
+            "This is for the java Logo, quite alright!"
+        ),
+        FactModel(
+            "Python",
+            R.drawable.python_logo,
+            "This is for the python Logo, quite alright!"
+        ),
+        FactModel(
+            "JavaScript",
+            R.drawable.js_logo,
+            "This is for the Java Script Logo, quite alright!"
+        ),
+        FactModel(
+            "PHP",
+            R.drawable.php_logo,
+            "This is for the php Logo, quite alright!"
+        ),
+    )
+}
---
+++ app/src/main/java/com/iameben/zuriviewgroups/FactModel.kt
@@ -0,0 +1,3 @@
+package com.iameben.zuriviewgroups
+
+data class FactModel(val name: String, val logo: Int, val fact: String)
