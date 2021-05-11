39bf4a74bcbbc0ac6764129b1962f097a27d3b21
Ebenezer Justice <justiceebenezer4@gmail.com>
2021-05-11 04:19:43 +0100

    init

---
+++ app/src/main/java/com/iameben/zuriviewgroups/FactAdapter.kt
@@ -0,0 +1,26 @@
+package com.iameben.zuriviewgroups
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class FactAdapter(context: Context, facts:List<FactModel>): ArrayAdapter<FactModel>(context, 0, facts) {
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view = convertView
+        val fact = getItem(position)
+        if(view == null){
+            view = LayoutInflater.from(parent.context).inflate(R.layout.fact_item, parent, false)
+        }
+        val logo = view?.findViewById<ImageView>(R.id.image_logo)
+        val name = view?.findViewById<TextView>(R.id.name)
+
+        return view!!
+    }
+
+}
