d78644f713c0b1630c76d772a4ad0913f0932baf
Ebenezer Justice <justiceebenezer4@gmail.com>
2021-05-14 04:33:30 +0100

    init

--- app/src/main/java/com/iameben/zuriviewgroups/LoginActivity.kt
+++ app/src/main/java/com/iameben/zuriviewgroups/LoginActivity.kt
@@ -1,21 +1,41 @@
 package com.iameben.zuriviewgroups
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        init()
     }
 
-    val login_button = findViewById<Button>(R.id.login_bt)
-    val email_tx = findViewById<EditText>(R.id.email_et)
-    val password_tx = findViewById<EditText>(R.id.password_et)
+    // initialization for the Ui components and onClickListener
+    private fun init(){
+        val click = findViewById<Button>(R.id.login_bt)
+        val email = findViewById<EditText>(R.id.email_et)
+        val password = findViewById<EditText>(R.id.password_et)
 
+        click.setOnClickListener{
+            // converts edittext input to string and compares with expected
+            validate(email.text.toString(), password.text.toString())
+        }
+    }
 
+    //  function to validate the user's input
+    private fun validate(userName: String, userPassword: String) {
+        if(userName == "test@test.com" && userPassword == "test"){
+            val intent = Intent(this, MainActivity::class.java)
+            Toast.makeText(applicationContext, R.string.toast_msg,Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+        }
 
+    }
 }
