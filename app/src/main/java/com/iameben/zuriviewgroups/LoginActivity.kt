d70af336faf9e6727a9fc7a3c921e0726fdbcbf9
Ebenezer Justice <justiceebenezer4@gmail.com>
2021-05-14 03:18:22 +0100

    init

--- app/src/main/java/com/iameben/zuriviewgroups/LoginActivity.kt
+++ app/src/main/java/com/iameben/zuriviewgroups/LoginActivity.kt
@@ -2,10 +2,20 @@ package com.iameben.zuriviewgroups
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+    val login_button = findViewById<Button>(R.id.login_bt)
+    val email_tx = findViewById<EditText>(R.id.email_et)
+    val password_tx = findViewById<EditText>(R.id.password_et)
+
+
+
 }
